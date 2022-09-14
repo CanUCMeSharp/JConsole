643b886b84e31ca94a9fa94e86b3e326d6ea6fa1
canUCMeSharp <tom.lochner07@outlook.com>
2022-09-14 18:50:12 +0200

    Added value Log and calueLogHASH File functionality

--- Console.java
+++ Console.java
@@ -7,39 +7,118 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public final class Console
 {
-    private static List<String> log =new ArrayList<String>();
-    private static String logHASH = "";
+    private static List<String> log = new ArrayList<String>();
+     static List<String> valueLog = new ArrayList<String>();
+    public static String logHASH = "";
+    public static String valueLogHASH = "";
+    private static File logFile = initLogFile();
     public static void log(String text){
-         System.out.println(text);
-         log.add(text);
-         try
-         {
-             logHASHUpdate();
-         }
-         catch (NoSuchAlgorithmException nsae)
-         {
-             nsae.printStackTrace();
-         }
+        System.out.println(text);
+        log.add(text);
+        try
+        {
+            logHASHDigest();
+        }
+        catch (NoSuchAlgorithmException nsae)
+        {
+            nsae.printStackTrace();
+        }
     }
+
     public static String read()throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
     }
-    private static void logHASHUpdate() throws NoSuchAlgorithmException {
+
+    public static void logValue(String value){
+        valueLog.add(value);
+        try
+        {
+            valueLogHASHDigest();
+        }
+        catch (NoSuchAlgorithmException nsae)
+        {
+            nsae.printStackTrace();
+        }
+    }
+
+    public static void fileSaveValueLog(){
+            try {
+                FileWriter writer = new FileWriter(logFile.toString(), true);
+                writer.write(valueLogHASH + '\n');
+                writer.close();
+            } catch (IOException e) {
+                Console.log("An error occurred.");
+                e.printStackTrace();
+            }
+    }
+
+    public static void fileReadValueLog(){
+        try {
+            Scanner myReader = new Scanner(logFile);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    
+    private static File initLogFile(){
+        String path = null;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.indexOf("windows")>-1) {
+            path = System.getenv("APPDATA");
+        } else if (osName.indexOf("mac")>-1) {
+            path = System.getenv("?");
+        } else {
+            path = System.getProperty("user.home");
+        }
+        try {
+            File logFileN = new File(path + "\\Log.txt");
+            if (logFileN.createNewFile()) {
+            } else {
+            }
+            return logFileN;
+        } catch (IOException e) {
+            Console.log("An error occurred in the File Creation process");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void logHASHDigest() throws NoSuchAlgorithmException {
         String cache = "";
         for(int i = 0; i < log.size(); i++){
             cache += log.get(i);
         }
         logHASH = toHexString(getSHA(cache));
     }
+
+    private static void valueLogHASHDigest() throws NoSuchAlgorithmException {
+        String cache = "";
+        for(int i = 0; i < valueLog.size(); i++){
+            cache += valueLog.get(i);
+        }
+        valueLogHASH = toHexString(getSHA(cache));
+    }
+
     private static byte[] getSHA(String input) throws NoSuchAlgorithmException
     {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         return md.digest(input.getBytes(StandardCharsets.UTF_8));
     }
+
     private static String toHexString(byte[] hash)
     {
         BigInteger number = new BigInteger(1, hash);
--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 public class Test
 {
     public void test() throws java.io.IOException {
-        Console.log("Test");
-        Console.log(Console.read());
+        Console.saveValueLog();
     }
 }
